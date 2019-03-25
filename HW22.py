e2e8d7ce8a9c8fe3f1a919f066905e9c417fd0b5
scott orban <scottjorban@gmail.com>
2019-03-24 21:21:48 -0600

    homework 22

---
+++ HW22.py
@@ -0,0 +1,61 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next = None
+        
+class linkedList:
+    def __init__(self):
+        self.head = None
+    def append(self, data):
+        newNode=Node(data)
+        if self.head==None:
+            self.head=newNode
+            return
+        else:
+            lastNode=self.head
+            while lastNode.next != None:
+                lastNode=lastNode.next
+            lastNode.next=newNode
+    def prepend(self, data): 
+        newNode=Node(data)
+        if self.head==None:
+            self.head=newNode
+            return
+        else:
+            newNode.next=self.head
+            self.head=newNode
+    def insertAfterNode(self,prevNode,data):
+        newNode=Node(data)
+        newNode.next=prevNode.next
+        prevNode.next=newNode
+    def printList(self):
+        curNode=self.head
+        while curNode!= None:
+            print(curNode.data)
+            curNode=curNode.next
+    def deleteNode(self, key):
+        curNode=self.head
+        if curNode!=None and curNode.data==key:
+            self.head=curNode.next
+            curNode=None
+            return
+        else:
+            prev=None
+            while curNode != None and curNode.data != key:
+                prev=curNode
+                curNode=curNode.next
+            if curNode==None:
+                print('The data is not found in the list')
+                return
+            else:
+                prev.next=curNode.next
+                curNode=None
+                
+list1=linkedList()
+list1.append('B')
+list1.insertAfterNode(list1.head,'C')
+list1.prepend('A')
+list1.printList()
+list1.deleteNode('B')
+list1.printList()
+list1.deleteNode('B')
